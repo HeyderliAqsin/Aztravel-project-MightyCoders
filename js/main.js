5dc6781e5ad6220e52a0898c04df3228f654f3d6
Abdurahmanov737 <anaraof@gmail.com>
2022-01-23 05:23:21 +0400

    23/01/2022

--- js/main.js
+++ js/main.js
@@ -1,34 +1,92 @@
-$("#header .header-search .search-top").click(function(){
+// //  this is for loading effect (start)
+// $(window).on("load", function () {
+//     setTimeout(() => {
+//         $("#myLoader").fadeOut("slow");
+//         $("body").css("overflow-y", "scroll")
+//     }, 2000);
 
-    var searchIcon=$(".search-icon");
-    var xIcon=$(".x-icon");
-    var drop=$(".search-dropdown");
+// });
+// //  this is for loading effect (end)
 
-    if(searchIcon.hasClass("active-icon")){
+// this is for scrollBtn (start)
+window.onscroll = function () {
+    scrollFunction();
+  
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $(".scrollBtn").css(
+            "bottom", "3%"
+        )
+  
+    } else {
+        $(".scrollBtn").css(
+            "bottom", "-150px"
+        )
+    }
+  }
+  
+  
+  
+  $(".scrollBtn").click(function () {
+    toTop();
+    var self = $(this);
+    $(this).addClass("activeBtn")
+    setTimeout(function () {
+        $(self).removeClass("activeBtn")
+    }, 2000)
+    
+  
+  })
+  
+  function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+// this is for scrollBtn (end)
+
+var headerDrop = $("#header .mobile-drop");
+
+$("#header .header-mobile").click(function(){
+
+    headerDrop.addClass("active-menu")
+
+})
+
+$("#header .mobile-drop .drop-close").click(function(){
+    headerDrop.removeClass("active-menu")
+    
+})
+
+$("#header .header-search .search-top").click(function () {
+
+    var searchIcon = $(".search-icon");
+    var xIcon = $(".x-icon");
+    var drop = $(".search-dropdown");
+
+    if (searchIcon.hasClass("active-icon")) {
         searchIcon.removeClass("active-icon");
         xIcon.addClass("active-icon")
-    }
-    else{
+    } else {
         searchIcon.addClass("active-icon");
         xIcon.removeClass("active-icon")
     }
 
-    if(drop.hasClass("active-dropdown")){
+    if (drop.hasClass("active-dropdown")) {
         drop.removeClass("active-dropdown")
-    }
-    else{
+    } else {
         drop.addClass("active-dropdown")
 
     }
 })
-  
+
 
 // this is the part where we change the playback speed of the video
 //----------------------------------------------------------//
-var myVideo=document.querySelector("#video video")
-myVideo.playbackRate=2.0
+var myVideo = document.querySelector("#video video")
+myVideo.playbackRate = 2.0
 //----------------------------------------------------------//
-
 
 
 //Tab menu
@@ -63,8 +121,18 @@ $('.Loc-Slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    arrows:false,
     autoplay:true,
     responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
         {
           breakpoint: 1024,
           settings: {
@@ -77,16 +145,66 @@ $('.Loc-Slider').slick({
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
           }
         }
       ]
   });
+
+
+  //Slick
+$('.Content').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay:true,
+  responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+});
+
+
+
